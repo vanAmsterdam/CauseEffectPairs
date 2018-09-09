79e2427cf84bc0c901290c27e3cc074762661ba6
vanAmsterdam <woutervanamsterdam@gmail.com>
2018-09-09 15:39:02 +0200

    added plot, first working edition; however, doesn't seem to update after retraining. Maybe detach?

--- model/net.py
+++ model/net.py
@@ -14,6 +14,19 @@ class TwoLayerNet(nn.Module):
         y_pred = self.linear2(h_relu)
         return y_pred
 
+class ThreeLayerNet(nn.Module):
+    def __init__(self, D_in, H1, H2, D_out):
+        super(ThreeLayerNet, self).__init__()
+        self.linear1 = nn.Linear(D_in, H1)
+        self.linear2 = nn.Linear(H1, H2)
+        self.linear3 = nn.Linear(H2, D_out)
+
+    def forward(self, x):
+        h1_relu = F.relu(self.linear1(x))
+        h2_relu = F.relu(self.linear2(h1_relu))
+        y_pred = self.linear3(h2_relu)
+        return y_pred
+
 def loss_fn(outputs, targets):
     return -torch.sum((outputs - targets)**2)
 
